4d80fe9abda9416ccc9b27c262e1b9497d51e54b
Horst Stumpf <Horststumpf@gmx.net>
2015-06-03 23:11:04 +0200

    first controller

--- src/SdcLight/Startup.cs
+++ src/SdcLight/Startup.cs
@@ -13,14 +13,16 @@ namespace SdcLight
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
+            // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
+            // services.AddWebApiConventions();
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            // Add MVC to the request pipeline.
+            app.UseMvc();
         }
     }
 }
